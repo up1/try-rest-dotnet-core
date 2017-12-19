7f1cd04c06b4fe52ba176021ba7b166027735507
somkiat <somkiat.p@gmail.com>
2017-12-19 13:57:52 +0700

    Create integration testing

---
+++ rest_testing/UnitTest1.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+using Newtonsoft.Json.Linq;
+using restapi.Models;
+
+namespace restapi.test
+{
+    public class UnitTest1
+    {
+
+        private readonly HttpClient client;
+        private readonly TestServer server;
+
+        public UnitTest1() {
+            server = new TestServer(
+                new WebHostBuilder().UseStartup<Startup>()
+            );
+            client = server.CreateClient();
+        }
+
+        [Fact]
+        public async void size_of_result_should_be_2()
+        {
+            var response = await client.GetAsync("/api/values");
+            response.EnsureSuccessStatusCode();
+
+            dynamic collection = JArray.Parse(await response.Content.ReadAsStringAsync());
+            Assert.Equal(2, collection.Count);
+        }
+
+        [Fact]
+        public async void first_user() {
+            var response = await client.GetAsync("/api/values");
+            response.EnsureSuccessStatusCode();
+
+            dynamic results = JArray.Parse(await response.Content.ReadAsStringAsync());
+            Assert.Equal(1, results[0].ToObject<User>().id);
+            Assert.Equal("User 1", results[0].ToObject<User>().name);
+        }
+    }
+}
---
+++ restapi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace restapi.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public User(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
